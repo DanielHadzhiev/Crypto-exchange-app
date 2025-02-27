a53bb4ea6686c598b4ff472613425482c199f553
DanielHadzhiev <danielhadziev1@gmail.com>
2025-02-27 16:41:09 +0200

    Deleting unnecessary files.

--- src/main/java/org/example/crypto/cryptoexchangeapp/service/CacheEvictionService.java
+++
@@ -1,7 +0,0 @@
-package org.example.crypto.cryptoexchangeapp.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CacheEvictionService {
-}
--- src/main/java/org/example/crypto/cryptoexchangeapp/service/impl/CacheEvictionServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package org.example.crypto.cryptoexchangeapp.service.impl;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CacheEvictionServiceImpl {
-}
